e0d3d3b6bede905483393bfcc0e181fb8dda40d6
alekseygalatin <alekse_galatin@epam.com>
2019-03-06 17:45:16 +0300

    Add project files.

---
+++ Geomapping/GeocodeService.cs
@@ -0,0 +1,63 @@
+﻿using GeneGenie.Geocoder;
+using GeneGenie.Geocoder.Models;
+using GeneGenie.Geocoder.Models.Geo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geomapping
+{
+    public class GeocodeService
+    {
+        private GeocodeManager geocodeManager;
+
+        public GeocodeService(string key)
+        {
+            var geocoderSettings = new List<GeocoderSettings>
+            {
+                new GeocoderSettings { ApiKey = key, GeocoderName = GeneGenie.Geocoder.Services.GeocoderNames.Google },
+            };
+
+            geocodeManager = GeocodeManager.Create(geocoderSettings);
+        }
+
+        public async Task<List<GeocodeResponseLocation>> GetLocationAsync(string address)
+        {
+            var response = await geocodeManager.GeocodeAddressAsync(address);
+            return response.Locations;
+        }
+
+        public double GetRadius(GeocodeResponseLocation location)
+        {
+            var distanceToNorthEast = GetDistanceFromLatLonInKm(location.Location.Latitude, location.Location.Longitude, location.Bounds.NorthEast.Latitude, location.Bounds.NorthEast.Longitude);
+            var distanceToSouthWest = GetDistanceFromLatLonInKm(location.Location.Latitude, location.Location.Longitude, location.Bounds.SouthWest.Latitude, location.Bounds.SouthWest.Longitude);
+
+            return Math.Max(distanceToNorthEast, distanceToSouthWest);
+        }
+
+        public double GetDistance(LocationPair location, LocationPair innerLocation)
+        {
+            return GetDistanceFromLatLonInKm(location.Latitude, location.Longitude, innerLocation.Latitude, innerLocation.Longitude);
+        }
+
+        private double GetDistanceFromLatLonInKm(double lat1, double lon1, double lat2, double lon2)
+        {
+            var radiusOfEarthInKm = 6371;
+            var dLat = DegToRad(lat2 - lat1);
+            var dLon = DegToRad(lon2 - lon1);
+            var a =
+              Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+              Math.Cos(DegToRad(lat1)) * Math.Cos(DegToRad(lat2)) *
+              Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+            var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            var distance = radiusOfEarthInKm * c;
+            return distance;
+        }
+
+        private double DegToRad(double deg)
+        {
+            return deg * (Math.PI / 180);
+        }
+    }
+}
---
+++ Geomapping/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Geomapping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var worker = new GeocodeService("AIzaSyDXrRQME2vYtdi94sWJV33Cl3A4mESSAXA");
+            Console.WriteLine("Input first location address:");
+            Console.WriteLine();
+            var address1 = Console.ReadLine();
+            var locations = worker.GetLocationAsync(address1).Result;
+            foreach (var location in locations)
+            {
+                Console.WriteLine($"Address of {location.FormattedAddress} has a location of {location.Location.Latitude} / {location.Location.Longitude}");
+                Console.WriteLine();
+
+                var radius = worker.GetRadius(location);
+
+                Console.WriteLine("Input second location address:");
+                Console.WriteLine();
+
+                var address2 = Console.ReadLine();
+                var innerLocations = worker.GetLocationAsync(address2).Result;
+                foreach (var innerLocation in innerLocations)
+                {
+                    Console.WriteLine($"Address of {innerLocation.FormattedAddress} has a location of {innerLocation.Location.Latitude} / {innerLocation.Location.Longitude}");
+                    Console.WriteLine();
+
+                    var distance = worker.GetDistance(location.Location, innerLocation.Location);
+                    var isLocatiomExist = distance < radius;
+
+                    Console.WriteLine($"Does {address1} include {address2} in radius of {radius}km: {isLocatiomExist}");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
